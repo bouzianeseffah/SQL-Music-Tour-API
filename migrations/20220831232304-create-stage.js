5e1be958132ac70c9e9ac65d661836b8ecad6399
bouzianes <fcbdreams1991@gmail.com>
2022-09-08 09:40:43 -0400

    adding routes

--- migrations/20220831232304-create-stage.js
+++ migrations/20220831232304-create-stage.js
@@ -1,30 +1,22 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Stages', {
-      id: {
+    await queryInterface.createTable('stages', {
+      stage_id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      stage_id: {
-        type: Sequelize.INTEGER
-      },
+      
       name: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.STRING,
+        allowNull: false
       }
+     
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Stages');
+    await queryInterface.dropTable('stages');
   }
 };
